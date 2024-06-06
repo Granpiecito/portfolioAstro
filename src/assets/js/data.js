b7d7a06ef1253cfcd97ff9041245be9ff3d6a970
jeanpierresperrymanaleman <jeanpierresperrymanaleman@gmail.com>
2024-06-06 22:46:15 +0000

    Added code in technologie.astro, need to implement local image in data.js

--- src/assets/js/data.js
+++ src/assets/js/data.js
@@ -15,3 +15,81 @@ export const info = [
     {text:'About', },
     {text:'Projects', }
 ];
+
+export const technologies = [
+    {
+        text: 'Java',
+        logo: 'bx bxl-java'
+    },
+    {
+        text: 'C++',
+        logo: '../img/Technologies/Languages/csharp.svg'
+    },
+    {
+        text: 'Html',
+        logo: '../img/Technologies/Languages/html5.svg'
+    },
+    {
+        text: 'Css',
+        logo: '../img/Technologies/Languages/css.svg'
+    },
+    {
+        text: 'Php',
+        logo: '../img/Technologies/Languages/Php_dark.svg'
+    },
+    {
+        text: 'Flutter',
+        logo: '../img/Technologies/Languages/flutter.svg'
+    },
+    {
+        text: 'Dart',
+        logo: '../img/Technologies/Languages/dart.svg'
+    },
+    {
+        text: 'Typescript',
+        logo: '../img/Technologies/Languages/typescript.svg'
+    },
+
+];
+
+export const frameworks = [
+    {
+        text: 'Astro',
+        logo: '../img/Technologies/Frameworks/Astro_dark.svg'
+    },
+    {
+        text: 'Boostrap',
+        logo: '../img/Technologies/Frameworks/boostrap.svg'
+    },
+    {
+        text: 'Laravel',
+        logo: '../img/Technologies/Frameworks/laravel.svg'
+    },
+    {
+        text: 'Springboot',
+        logo: '../img/Technologies/Frameworks/spring.svg'
+    },
+    {
+        text: 'Svelte',
+        logo: '../img/Technologies/Frameworks/svelte.svg'
+    },
+    {
+        text: 'Tailwind Css',
+        logo: '../img/Technologies/Frameworks/tailwindcss.svg'
+    },
+];
+
+export const dataBase = [
+    {
+        text: 'Mysql',
+        logo: '../img/Technologies/Data Base/mysql.svg'
+    },
+    {
+        text: 'Postgresql',
+        logo: '../img/Technologies/Data Base/postgresql.svg'
+    },
+    {
+        text: 'Sql Server',
+        logo: '../img/Technologies/Data Base/sql-server.svg'
+    },
+];
